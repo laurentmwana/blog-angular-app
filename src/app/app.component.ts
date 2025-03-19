6a6dc0291c9d2314c9bd8692b4914b20393f7bbd
Labeya <laurentmwn@gmail.com>
2025-03-19 13:39:17 +0100

    feat: add component list-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ArticleComponent } from './article/article.component';
+import { ListArticleComponent } from './list-article/list-article.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListArticleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'blog-app';
 }
---
+++ src/app/list-article/list-article.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ArticleComponent } from '../article/article.component';
+
+@Component({
+  selector: 'app-list-article',
+  imports: [ArticleComponent],
+  templateUrl: './list-article.component.html',
+  styleUrl: './list-article.component.css'
+})
+export class ListArticleComponent {
+
+}
