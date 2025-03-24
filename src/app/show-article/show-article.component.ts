660580ce4f2ff744a3dabc0c6a52210082e7d89b
Labeya <laurentmwn@gmail.com>
2025-03-24 16:51:01 +0100

    feat: install tailwind

---
+++ src/app/show-article/show-article.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-article',
+  imports: [],
+  templateUrl: './show-article.component.html',
+  styleUrl: './show-article.component.css'
+})
+export class ShowArticleComponent {
+
+}
